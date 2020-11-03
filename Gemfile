da6935655a243efaea601309bc85e4828e25ba0d
JuliKroiter <kroiter09@gmail.com>
2020-11-03 22:20:45 +0300

    Add Rapid theme

--- Gemfile
+++ Gemfile
@@ -44,6 +44,7 @@ gem 'devise'
 gem 'bootstrap', '~> 4.0.0'
 gem 'mini_racer', platforms: :ruby
 gem 'jquery-rails'
+gem 'owlcarousel-rails'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,7 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require jquery
 //= require rails-ujs
 //= require activestorage
 //= require turbolinks
@@ -17,7 +18,46 @@
 //= require jquery3
 //= require popper
 //= require bootstrap
+//= require main
+//= require counterup/counterup.min
+//= require owl.carousel
+//= require venobox
+//= require isotope.pkgd
+//= require aos
 
-$('#myModal').on('shown.bs.modal', function () {
-    $('#myInput').trigger('focus')
-})
+$(document).ready(function() {
+    // Testimonials carousel (uses the Owl Carousel library)
+    $(".testimonials-carousel").owlCarousel({
+        autoplay: true,
+        dots: true,
+        loop: true,
+        items: 1
+    });
+
+// Clients carousel (uses the Owl Carousel library)
+    $(".clients-carousel").owlCarousel({
+        autoplay: true,
+        dots: true,
+        loop: true,
+        responsive: {
+            0: {
+                items: 2
+            },
+            768: {
+                items: 4
+            },
+            900: {
+                items: 6
+            }
+        }
+    });
+
+// Portfolio details carousel
+    $(".portfolio-details-carousel").owlCarousel({
+        autoplay: true,
+        dots: true,
+        loop: true,
+        items: 1
+    });
+
+});
