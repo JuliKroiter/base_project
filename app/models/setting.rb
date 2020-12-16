fd3f9ab891953ac5ed00477b7857cfa5b557fdd4
JuliKroiter <kroiter09@gmail.com>
2020-12-16 14:33:21 +0300

    Update settings add project name and get started

--- app/models/setting.rb
+++ app/models/setting.rb
@@ -1,12 +1,12 @@
 # RailsSettings Model
 class Setting < RailsSettings::Base
   cache_prefix { "v1" }
-  SETTING_FIELDS = %w{ main_title about_company_title about_company_description about_company_content
+  SETTING_FIELDS = %w{ main_title main_get_starred about_company_title about_company_description about_company_content
     services_title services_description sales_title price_title price_description portfolio_title
     about_us_title about_us_description about_us_content
     counter_1_title counter_1_value counter_2_title counter_2_value counter_3_title counter_3_value counter_4_title counter_4_value
     clients_title team_title team_description faq_title faq_description
-    footer_title footer_description subscribers_title subscribers_description address email phone request_title
+    project_name footer_description subscribers_title subscribers_description address email phone request_title
     request_description vk_link instagram_link facebook_link youtube_link
 }
 
