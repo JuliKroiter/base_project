1e01cf1bbe6520323295268af4e18177d040f543
JuliKroiter <kroiter09@gmail.com>
2020-12-16 14:46:49 +0300

    Finish index blocks

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,6 +5,9 @@ class HomeController < ApplicationController
     @services = Service.where(active: true)
     @portfolio_categories = PortfolioCategory.joins(:portfolios)
     @portfolios = Portfolio.where(active: true)
+    @sales = Sale.where(active: true)
+    @employers = Employee.where(active: true)
+    @clients = Client.where(active: true)
     @faqs = Faq.where(active: true)
   end
 end
