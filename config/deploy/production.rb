bc34db51dbe86b2274965142d5cf671da0559920
JuliKroiter <kroiter09@gmail.com>
2021-03-29 23:14:30 +0300

    Fix puma production path

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -77,7 +77,7 @@ namespace :deploy do
   desc 'Initial Deploy'
   task :initial do
     on roles(:app) do
-      before 'deploy:restart', 'puma:start'
+      invoke("puma:start")
       invoke 'deploy'
     end
   end
