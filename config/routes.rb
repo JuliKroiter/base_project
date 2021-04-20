aedfdb0801859c6d62dc36b6fbbf5a120c1e983b
JuliKroiter <kroiter09@gmail.com>
2021-04-20 19:01:05 +0300

    Add error pages

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,10 @@ Rails.application.routes.draw do
   resources :subscriptions, only: [:create]
 
   root to: 'home#index'
+
+  get '/404', to: "errors#not_found"
+  get '/422', to: "errors#unacceptable"
+  get '/500', to: "errors#internal_error"
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
